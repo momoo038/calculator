c64f0db377acab62b7b9a128d33aca14a7a2840a
momoo038 <196245462+momoo038@users.noreply.github.com>
2025-03-15 17:55:17 +0100

    Added calculator logic

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+let number1 = 0;
+let number2 = 0;
+let operator;
+
 let add = function (a, b) {
   return a + b;
 };
@@ -15,11 +19,7 @@ let divide = function (a, b) {
 };
 
 let display = document.querySelector("#display");
-display.value = "0123456789";
-
-let number1 = 0;
-let number2 = 0;
-let operator;
+display.value = "";
 
 let operate = function (operator, a, b) {
   switch (operator) {
@@ -34,12 +34,22 @@ let operate = function (operator, a, b) {
   }
 };
 
-let appendToDisplay = function (number) {
-  display.value = display.value + number;
+let appendToDisplay = function (input) {
+  display.value = display.value + input;
   display.style = "text-align: right";
-  ("overflow: hidden");
 };
 
 let clearDisplay = function () {
   display.value = "";
+  operatorEntered = false;
+};
+
+let calculate = function () {
+  let strToNum = display.value.split(operator);
+  if (strToNum.length !== 2) {
+    return;
+  }
+  const [num1, num2] = strToNum.map(Number);
+  display.value = operate(operator, num1, num2);
+  operatorEntered = false;
 };
