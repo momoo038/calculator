e6099ba01242becab86d4b9df07208a6df162684
momoo038 <196245462+momoo038@users.noreply.github.com>
2025-03-15 17:08:18 +0100

    Made display register input

--- calculator.js
+++ calculator.js
@@ -1,35 +1,45 @@
 let add = function (a, b) {
-    return a + b;
+  return a + b;
 };
 
 let subtract = function (a, b) {
-    return a - b;
-}
+  return a - b;
+};
 
 let multiply = function (a, b) {
-    return a * b;
-}
+  return a * b;
+};
 
 let divide = function (a, b) {
-    return a / b;
-}
+  return a / b;
+};
 
-let display = document.querySelector('#display');
+let display = document.querySelector("#display");
 display.value = "0123456789";
 
 let number1 = 0;
 let number2 = 0;
-let operator
+let operator;
 
 let operate = function (operator, a, b) {
-    switch (operator) {
-        case '+':
-            return add(a, b);
-        case '-':
-            return subtract(a, b);
-        case '*':
-            return multiply(a, b);
-        case '/':
-            return divide(a, b);
-    }
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+  }
+};
+
+let appendToDisplay = function (number) {
+  display.value = display.value + number;
+  display.style = "text-align: right";
+  ("overflow: hidden");
+};
+
+let clearDisplay = function () {
+  display.value = "";
 };
